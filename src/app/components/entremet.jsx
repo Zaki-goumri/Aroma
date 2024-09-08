601c2577bc20edb07dac1e716c9ea470bca37f94
zaki <z.goumri@esi-sba.dz>
2024-09-08 19:08:17 +0100

    rsponsive

--- src/app/components/entremet.jsx
+++ src/app/components/entremet.jsx
@@ -1,11 +1,11 @@
 
 const Entremet = () => {
   return (
-    <div className="flex md:justify-around  md:gap-10 md:flex-row items-center md:mx-[100px] my-10  gap-4 flex-col-reverse">
+    <div className="flex md:justify-around  md:gap-10 lg:flex-row items-center md:mx-[100px] my-10  gap-4 flex-col-reverse">
    
-    <aside className="flex flex-col items-start gap-7">
-      <h1 className="font-bold  text-2xl md:text-4xl max-w-sm text-white mb-3">Special Orders</h1>
-      <p className="max-w-xs md:leading-normal leading-6 ">Lorem ipsum dolor sit amet consectetur. Dictum et at posuere dolor lacus sed urna nisl. Fermentum et purus lectus nisi gravida morbi vitae condimentum ullamcorper. Justo diam duis in ullamcorper aliquam fringilla et sed semper. Sapien magna sodales nibh sapien viverra neque gravida risus.</p>
+    <aside className="flex flex-col items-start gap-7  pl-2 xxs:pl-4 xs:pl-6">
+      <h1 className="font-bold  text-2xl lg:text-4xl max-w-sm text-white mb-3">Special Orders</h1>
+      <p className=" lg:leading-normal leading-6  max-w-screen-xs">Lorem ipsum dolor sit amet consectetur. Dictum et at posuere dolor lacus sed urna nisl. Fermentum et purus lectus nisi gravida morbi vitae condimentum ullamcorper. Justo diam duis in ullamcorper aliquam fringilla et sed semper. Sapien magna sodales nibh sapien viverra neque gravida risus.</p>
       <button className="bg-stone-300 text-aroma rounded-[50px] w-44 h-16 font-bold mt-6 ">Contactez-nous</button>
       </aside>
     <img src="/patisserie/entremet2.png" alt="entremet" height={400} width={420} className="mb-4 sm:px-4 px-6" />
