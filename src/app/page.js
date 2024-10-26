04be3a40553d14690824f8f8f06821ea365d5599
Zaki-goumri <153986120+Zaki-goumri@users.noreply.github.com>
2024-10-26 13:50:40 +0100

    Update page.js
    priority

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center ">
     <motion.div variants={heroVar} initial="hidden" animate="visible"  className="flex md:justify md:gap-10 lg:flex-row items-center md:mx-[100px] my-10 flex-col gap-4 mb-0 ">
-      <Image src="/aroma/piecemontee.jpg" alt="entremet" height={380} width={420} className="mb-4 md:px-0 px-6 rounded-[60px] "priority='false' />
+      <Image src="/aroma/piecemontee.jpg" alt="entremet" height={380} width={420} className="mb-4 md:px-0 px-6 rounded-[60px]" priority />
       <aside className="flex flex-col items-start gap-7 xs:pl-4 pl-6">
         <h1 className="font-bold  text-2xl md:text-4xl max-w-sm text-aroma mb-3   ">Aroma, une pâtisserie </h1>
         <p className="max-w-xs md:leading-normal leading-6 font-medium ">Artisanat, passion et saveurs.Nous vous proposons des pâtisseries artisanales, élaborées avec des ingrédients frais et locaux, dans une ambiance chaleureuse. Laissez-vous tenter par nos délices !
